b33cb6577f7398261ad42f32d2e7bbb6df482a53
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-26 22:11:43 -0600

    insertando usuarios en BD

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,19 @@
-exports.crearUsiario = (req, res) => {
-  console.log(req.body);
+const Usuario = require("../models/Usuario");
+
+exports.crearUsiario = async (req, res) => {
+  try {
+    let usuario;
+
+    // Guardar el nuevo usuaro
+    usuario = new Usuario(req.body);
+
+    // Guarda el usuario
+    await usuario.save();
+
+    // Mensaje de confirmacion
+    res.send("Usuario guardado correctamente");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Hubo un error");
+  }
 };
