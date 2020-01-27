f04822cff3919d8094eee799ff768ada792f0dde
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-26 22:18:17 -0600

    revisando si el usuario ya no ha sido registrado en BD

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,16 @@
 const Usuario = require("../models/Usuario");
 
 exports.crearUsiario = async (req, res) => {
+  // Extraer email y password
+  const { email, password } = req.body;
+
   try {
-    let usuario;
+    // REvisar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
 
     // Guardar el nuevo usuaro
     usuario = new Usuario(req.body);
@@ -11,7 +19,7 @@ exports.crearUsiario = async (req, res) => {
     await usuario.save();
 
     // Mensaje de confirmacion
-    res.send("Usuario guardado correctamente");
+    res.json({ msg: "Usuario creado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
