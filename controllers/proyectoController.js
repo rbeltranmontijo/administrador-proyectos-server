2e90fa8349cba8522fe44cd48e0d02a01039f394
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-27 21:50:33 -0600

    Verificando si el usuario esta autenticado para agregar proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,1 +1,19 @@
-exports.crearProyecto = async (req, res) => {};
+const Proyecto = require("../models/Proyecto");
+
+exports.crearProyecto = async (req, res) => {
+  try {
+    // Crear un nuevo proyecto
+    const proyecto = new Proyecto(req.body);
+
+    // Guardar el creador via JWT
+    proyecto.creador = req.usuario.id;
+
+    //Guardar el proyecto
+    proyecto.save();
+
+    res.json(proyecto);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
