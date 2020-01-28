b4116887919f859e359561fe72443e2e3157e420
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-27 22:21:29 -0600

    revisando si un proyecto existe para editarlo

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -55,6 +55,26 @@ exports.actualizarProyecto = async (req, res) => {
   }
 
   try {
+    // Revisar id
+    let proyecto = await Proyecto.findById(req.params.id);
+
+    // Si el proyecto existe o no
+    if (!proyecto) {
+      return res.status(404).json("Proyecto no encontrado");
+    }
+
+    // Verificar el creador del proyecto
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json("No autorizado");
+    }
+
+    // Actualizar
+    proyecto = await Proyecto.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: nuevoProyecto },
+      { new: true }
+    );
+    res.json({ proyecto });
   } catch (error) {
     console.log(error);
     res.status(500).send("Error en el servidor");
