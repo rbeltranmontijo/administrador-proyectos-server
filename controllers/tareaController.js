745119f1ecb3446923d5da351bef413b8a78478a
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-28 22:29:37 -0600

    actualizar tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -40,7 +40,7 @@ exports.obtenerTareas = async (req, res) => {
     // Extraer el proyecto y comprobar si existe
     const { proyecto } = req.body;
 
-    const existeProyecto = await Proyecto.findById(proyecto);
+    let existeProyecto = await Proyecto.findById(proyecto);
     if (!existeProyecto) {
       return res.status(404).json({ msg: "Proyecto no encontrado" });
     }
@@ -53,6 +53,43 @@ exports.obtenerTareas = async (req, res) => {
     //Obtener las tareas por proyecto
     const tareas = await Tarea.find({ proyecto });
     res.json({ tareas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+// ACtualizar una tarea
+exports.actualizarTarea = async (req, res) => {
+  try {
+    // Extraer el proyecto y comprobar si existe
+    const { proyecto, nombre, estado } = req.body;
+
+    //Extraer proyecto
+    let tarea = await Tarea.findById(req.params.id);
+
+    if (!tarea) {
+      return res.status(404).json({ msg: "No existe la tarea" });
+    }
+
+    // Extraer proyecto
+    let existeProyecto = await Proyecto.findById(proyecto);
+
+    // REvisar si el proyecto actual pertenece al usuario autenticado
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json("No autorizado");
+    }
+
+    // Crear un objeto con la nuevo informacion
+    const nuevaTarea = {};
+    if (nombre) nuevaTarea.nombre = nombre;
+    if (estado) nuevaTarea.estado = estado;
+
+    //Guardar la tarea
+    tarea = await Tarea.findOneAndUpdate({ _id: req.params.id }, nuevaTarea, {
+      new: true
+    });
+    res.json({ tarea });
   } catch (error) {
     console.log(error);
     res.status(500).send("Hubo un error");
