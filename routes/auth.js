3ffae05126a360ba20d315380a15832816603cac
Robin Beltran <rbeltranmontijo@gmail.com>
2020-01-29 23:07:22 -0600

    agregando rota get para obtener al usuario autenticado

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
-// Crea un usuario
+// Iniciar sesion
 // api/auth
 router.post(
   "/",
@@ -16,5 +17,8 @@ router.post(
   ],
   authController.autenticarUsuario
 );
+
+//Obtiene el usuario autenticado
+router.get("/", auth, authController.usuarioAutenticado);
 
 module.exports = router;
