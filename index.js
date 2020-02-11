7721fa60c38c715804217e6ea0fac1214ccadaec
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-10 22:12:58 -0600

    ajustes deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -24,6 +24,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 // Arrancando la app
-app.listen(port, "0.0.0.0", () => {
-  console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
